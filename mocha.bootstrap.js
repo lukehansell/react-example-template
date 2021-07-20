144e0e96d1eb624aaf3d91eb7c82471f3bd94e1f
Luke Hansell <lukehansell@gmail.com>
2021-07-20 17:42:34 +0100

    add sinon and sinon-chai

--- mocha.bootstrap.js
+++ mocha.bootstrap.js
@@ -3,8 +3,10 @@ import chaiEnzyme from 'chai-enzyme'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import React from 'react'
+import sinonChai from 'sinon-chai'
 
 chai.use(chaiEnzyme())
+chai.use(sinonChai)
 
 Enzyme.configure({
   adapter: new Adapter()
